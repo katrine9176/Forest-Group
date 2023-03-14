7301553af4b16ac6349b35f52acd96e3295ba58e
Alen <grafuxa@gmail.com>
2023-03-14 21:31:22 +0100

    set single quotes

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,39 @@
-const burgerButton = document.querySelector(".nav__burger--burger");
-const close = document.querySelector(".nav__burger--close");
-const linksBox = document.querySelector(".nav__links");
-const links = document.querySelectorAll(".nav__links-link");
-const footerYear = document.querySelector(".footer__year-date");
-const userName = document.querySelector("#name");
-const userSurname = document.querySelector("#surname");
-const mail = document.querySelector("#mail");
-const formMessage = document.querySelector("#content");
-const confirmFormbutton = document.querySelector(".contact__form-btn");
-const notification = document.querySelector(".contact__form-error");
+const burgerButton = document.querySelector('.nav__burger--burger');
+const close = document.querySelector('.nav__burger--close');
+const linksBox = document.querySelector('.nav__links');
+const links = document.querySelectorAll('.nav__links-link');
+const footerYear = document.querySelector('.footer__year-date');
+const userName = document.querySelector('#name');
+const userSurname = document.querySelector('#surname');
+const mail = document.querySelector('#mail');
+const formMessage = document.querySelector('#content');
+const confirmFormbutton = document.querySelector('.contact__form-btn');
+const notification = document.querySelector('.contact__form-error');
 
 const handleNav = () => {
-	linksBox.classList.add("handle-nav");
-	document.body.classList.add("overflow-y");
-	burgerButton.style.display = "none";
-	close.style.display = "block";
+	linksBox.classList.add('handle-nav');
+	document.body.classList.add('overflow-y');
+	burgerButton.style.display = 'none';
+	close.style.display = 'block';
 
 	const closeNav = () => {
-		linksBox.classList.remove("handle-nav");
-		document.body.classList.remove("overflow-y");
-		burgerButton.style.display = "block";
-		close.style.display = "none";
+		linksBox.classList.remove('handle-nav');
+		document.body.classList.remove('overflow-y');
+		burgerButton.style.display = 'block';
+		close.style.display = 'none';
 	};
 
-	close.addEventListener("click", closeNav);
+	close.addEventListener('click', closeNav);
 	links.forEach((link) => {
-		link.addEventListener("click", closeNav);
+		link.addEventListener('click', closeNav);
 	});
 };
 
 const setFormNotificationMessage = (message, isError = false) => {
 	notification.textContent = message;
-	notification.style.color = isError ? "red" : "green";
+	notification.style.color = isError ? 'red' : 'green';
 	setTimeout(() => {
-		notification.textContent = "";
+		notification.textContent = '';
 	}, 2000);
 };
 
@@ -45,19 +45,19 @@ const checkForm = () => {
 	const isEmailCorrect = emailRegexp.test(mail.value);
 	const hasError = true;
 	if (formCondition) {
-		setFormNotificationMessage("");
+		setFormNotificationMessage('');
 	} else {
-		setFormNotificationMessage("Uzupełnij wszystkie pola!", hasError);
+		setFormNotificationMessage('Uzupełnij wszystkie pola!', hasError);
 	}
 	if (!isEmailCorrect) {
-		setFormNotificationMessage("Nieprawidłowy email!", hasError);
+		setFormNotificationMessage('Nieprawidłowy email!', hasError);
 	}
 	if (isEmailCorrect && formCondition) {
-		userName.value = "";
-		userSurname.value = "";
-		mail.value = "";
-		formMessage.value = "";
-		setFormNotificationMessage("Wiaodmość wysłana!", !hasError);
+		userName.value = '';
+		userSurname.value = '';
+		mail.value = '';
+		formMessage.value = '';
+		setFormNotificationMessage('Wiaodmość wysłana!', !hasError);
 	}
 };
 
@@ -67,9 +67,9 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year;
 };
 
-burgerButton.addEventListener("click", handleNav);
+burgerButton.addEventListener('click', handleNav);
 
-confirmFormbutton.addEventListener("click", (e) => {
+confirmFormbutton.addEventListener('click', (e) => {
 	e.preventDefault();
 	checkForm();
 });
